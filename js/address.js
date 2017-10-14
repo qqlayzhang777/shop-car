b00dbc6b148ed8abf9d161922f022d495e4a0fbf
qqlayzhang777 <31338995+qqlayzhang777@users.noreply.github.com>
2017-10-14 10:52:02 +0800

    Add files via upload

---
+++ js/address.js
@@ -0,0 +1,72 @@
+new Vue({
+	el: '.container',
+	data: {
+		addressList: [],
+		// checked: false,
+		checked: 0,
+		addressNum: 3,
+		expressMethod: 0
+		// asd: ''
+	},
+	mounted: function () {
+		this.$nextTick(function () {
+			this.addressView()
+		})
+	},
+	computed: {
+		filterAddress: function () {
+			return this.addressList.slice(0, this.addressNum)
+		}
+	},
+	methods: {
+		addressView: function () {
+			var _this = this
+			this.$http.get('data/address.json').then(function (response) {
+					var res = response.data
+					if (res.status == 0) {
+						_this.addressList = res.result
+					}
+			})
+		},
+		// checkD: function (item, index) {
+		//  if(typeof item.checked == 'undefined') {
+		//    this.$set(item, 'checked', false)
+		//    this.addressList[0].checked = true
+		//  }
+		// },
+		// checkIt: function (item) {
+		//  this.addressList.forEach(function (value, index) {
+		//    value.checked = false
+		//  })
+		//  item.checked = true
+		// },
+		showMore: function () {
+			if (this.addressNum == this.addressList.length) {
+				this.addressNum = 3
+			} else {
+				this.addressNum = this.addressList.length
+			}
+		},
+		isDefaultItem: function (item, index) {
+			this.addressList.forEach(function (value, index) {
+				value.isDefault = false
+			})
+			this.addressList.splice(index, 1)
+			this.addressList.unshift(item)
+			item.isDefault = true
+			this.checked = 0
+		}
+		// isDefaultItem: function (item, index) {
+		// 	this.addressList.forEach(function (value, index) {
+		// 		value.isDefault = false
+		// 	})
+		// 	this.addressList.splice(index, 1)
+		// 	this.addressList.unshift(item)
+		// 	item.isDefault = true
+		// 	this.addressList.forEach((item, index) => {
+		// 		item.checked = false
+		// 	  this.addressList[0].checked = true
+		// 	})
+		// }
+	}
+})
---
+++ js/cart.js
@@ -0,0 +1,119 @@
+var vm = new Vue({
+  el: '#app',
+  data: {
+    totalMoney: 0,
+    productList: [],
+    checkItems: true,
+    id: 0,
+    isEject: '',
+    isFlag: false
+  },
+  filters: {
+    formateMoney: function (value,type) {
+      if(type == undefined){
+        type = ''  //return '¥ ' + value.toFixed(2)
+      }
+      return '¥ ' + value.toFixed(2) + type
+    }
+  },
+  mounted: function () {
+    this.$nextTick(function () {
+      this.cartview()
+    })
+  },
+  methods: {
+    cartview: function () {
+      var _this = this
+      this.$http.get("data/cartData.json", {"id":123}).then(function (res) {
+        _this.productList = JSON.parse(res.body).result.list
+        // _this.totalMoney = JSON.parse(res.body).result.totalMoney
+      })
+    },
+    numP: function (item,int) {
+      if(int>0){
+        item.productQuantity++
+      }else{
+        item.productQuantity--
+        if(item.productQuantity<1){
+          item.productQuantity=1
+        }
+      }
+      this.totalPay()
+    },
+    checkD: function (item) {
+      if(typeof item.checked == 'undefined'){
+        Vue.set(item, "checked", true)
+        this.id = this.productList.length
+        this.totalPay()
+      }
+    },
+    checkIt: function (item) {
+      var _this = this  
+      item.checked = !item.checked
+      if(item.checked){
+        this.id++
+        if(this.id == _this.productList.length){
+          _this.checkItems = true
+        }
+      }else{
+        this.id--
+        _this.checkItems = false
+      }
+      this.totalPay()
+    },
+    checkAll: function (nu) {
+      var _this = this
+      this.checkItems = !this.checkItems
+      this.productList.forEach(function (item, index) {
+        if(nu>0){
+          _this.checkItems = true
+        }
+        if(nu<0){
+          _this.checkItems = false
+        }
+        item.checked = _this.checkItems
+      })
+      if(this.checkItems){
+        this.id = this.productList.length
+      }else{
+        this.id = 0
+      }
+      if(this.productList.length == 0){
+        this.checkItems = false
+      }
+      this.totalPay()
+    },
+    remove: function (item) {
+      this.isFlag = true
+      this.isEject = item
+    },
+    totalPay: function () {
+      var _this = this
+      this.totalMoney = 0
+      this.productList.forEach(function (item, index) {
+        if(item.checked){
+          _this.totalMoney += item.productPrice * item.productQuantity
+        }
+      })
+    },
+    onYes: function () {
+      var index = this.productList.indexOf(this.isEject)
+      this.productList.splice(index, 1)
+      if(this.isEject.checked){
+        this.id--
+      }
+      if(this.id == this.productList.length){
+        this.checkItems = true
+      }
+      if(this.productList.length == 0){
+        this.checkItems = false
+      }
+      this.totalPay()
+      this.isFlag = false
+    }
+  }
+})
+
+// Vue.filter("money", function (value,type) {
+//    return '¥ ' + value.toFixed(2) + type
+// })
